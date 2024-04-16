3dd0add34168064092480cec5788243159747ce4
jlopezds <162152018+jlopezds@users.noreply.github.com>
2024-04-16 10:31:31 -0600

    Update Package.swift platforms .ios .v12

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "DsToken",
-    platforms: [.iOS(.v11)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "DsToken",
