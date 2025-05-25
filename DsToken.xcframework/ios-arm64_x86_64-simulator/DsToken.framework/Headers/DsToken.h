6ae07856ce3f8a1841e6540dfaff745ec4ea4b71
jlopezds <162152018+jlopezds@users.noreply.github.com>
2025-05-25 10:17:53 -0600

    Add files via upload

---
+++ DsToken.xcframework/ios-arm64_x86_64-simulator/DsToken.framework/Headers/DsToken.h
@@ -0,0 +1,18 @@
+//
+//  DsToken.h
+//  DsToken
+//
+//  Created by Devel Systems on 15/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DsToken.
+FOUNDATION_EXPORT double DsTokenVersionNumber;
+
+//! Project version string for DsToken.
+FOUNDATION_EXPORT const unsigned char DsTokenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DsToken/PublicHeader.h>
+
+
